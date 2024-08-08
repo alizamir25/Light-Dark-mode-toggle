c75140cccc73ff7441210790af96a8688f26d06f
alizamir25 <ali0786zamir@gmail.com>
2024-08-08 13:43:07 +0100

    Major Patch
    Added save user preferences + code clean up

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggler = document.getElementById('dark-mode-toggler');
-
-    // Check for saved user preference in local storage
     if (localStorage.getItem('dark-mode') === 'enabled') {
         document.body.classList.add('dark-mode');
     }
-
     toggler.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
-
-        // Save user preference in local storage
         if (document.body.classList.contains('dark-mode')) {
             localStorage.setItem('dark-mode', 'enabled');
         } else {
