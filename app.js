33a11863d3906f0ed7976a6b6fe017eb917bf6dc
alizamir25 <ali0786zamir@gmail.com>
2024-08-08 13:46:46 +0100

    Patch
    Code clean up, size an color adjustments

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded',()=>{
     const toggler = document.getElementById('dark-mode-toggler');
-    if (localStorage.getItem('dark-mode') === 'enabled') {
+    if (localStorage.getItem('dark-mode') === 'enabled'){
         document.body.classList.add('dark-mode');
     }
-    toggler.addEventListener('click', () => {
+    toggler.addEventListener('click',()=>{
         document.body.classList.toggle('dark-mode');
-        if (document.body.classList.contains('dark-mode')) {
+        if (document.body.classList.contains('dark-mode')){
             localStorage.setItem('dark-mode', 'enabled');
-        } else {
+        }else{
             localStorage.setItem('dark-mode', 'disabled');
         }
     });
